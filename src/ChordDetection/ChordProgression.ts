8587dd0e4924cab98fd5210a7fbe4e64468562ee
Ivy G <ivymisc@posteo.net>
2021-06-04 19:01:13 -0700

    feat: :sparkles: Add chord progressions

--- src/ChordDetection/ChordProgression.ts
+++ src/ChordDetection/ChordProgression.ts
@@ -7,9 +7,26 @@ export const chordProgressions: ChordProgression[] = [
     {
         name: "Test progression",
         progression: ["IMaj7", "IIm7", "V7"],
-    },
-    {
+    }, {
         name: "ii-V-I progression",
         progression: ["ii", "V", "I"],
-    },
+    }, {
+        name: "I-IV-V-I progression",
+        progression: ["I", "IV", "V", "I"],
+    }, {
+        name: "I-ii-V-I progression",
+        progression: ["I", "ii", "V", "I"],
+    }, {
+        name: "I-vi-IV-V progression",
+        progression: ["I", "vi", "IV", "V"],
+    }, {
+        name: "I-vi-ii-V progression",
+        progression: ["I", "vi", "ii", "V"],
+    }, {
+        name: "I-IV-vi-V progression",
+        progression: ["I", "IV", "vi", "V"],
+    }, {
+        name: "I-V-vi-IV progression",
+        progression: ["I", "V", "vi", "IV"],
+    }
 ];
